471d68c4b6b3a30a693abdc0e80b7295ed4954fa
Sohaill123 <sohail.mnk6750@gmail.com>
2023-07-15 22:34:19 +0530

    changing js code

--- scripts.js
+++ scripts.js
@@ -1,72 +1,92 @@
-function goToMenuPage() {
-    document.getElementById('home-page').style.display = 'none';
-    document.getElementById('menu-page').style.display = 'block';
-    getMenu();
-  }
-  
-  function takeOrder() {
-    // Function to handle the order process
-  }
-  
-  function getMenu() {
+let items = document.getElementById('items')
+items.innerHTML = "";
+
+let topDiv = document.getElementById('top');
+function getMenu(){
     fetch('https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json')
-      .then(response => response.json())
-      .then(data => {
-        const menuContainer = document.getElementById('menu');
-        menuContainer.innerHTML = '';
-        data.forEach(item => {
-          const menuItem = document.createElement('div');
-          menuItem.className = 'menu-item';
-          menuItem.innerHTML = `
-            <img src="${item.image}" alt="${item.name}" />
-            <h3>${item.name}</h3>
-            <p>${item.description}</p>
-            <p>Price: $${item.price}</p>
-          `;
-          menuContainer.appendChild(menuItem);
-        });
-      })
-      .catch(error => console.log('Error:', error));
-  }
-  
-  // Functions for order processing: takeOrder(), orderPrep(), payOrder(), thankyouFnc()
-  
-  // Example of order processing flow
-  function takeOrder() {
-    // Simulating order processing using setTimeout and Promise
-    console.log('Taking order...');
-    setTimeout(() => {
-      console.log('Order processed.');
-      orderPrep()
-        .then(orderStatus => {
-          console.log('Order Status:', orderStatus);
-          return payOrder();
-        })
-        .then(paidStatus => {
-          console.log('Paid Status:', paidStatus);
-          return thankyouFnc();
-        })
-        .catch(error => console.log('Error:', error));
-    }, 2500);
-  }
-  
-  function orderPrep() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve({ order_status: true, paid: false });
-      }, 1500);
-    });
-  }
-  
-  function payOrder() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve({ order_status: true, paid: true });
-      }, 1000);
-    });
-  }
-  
-  function thankyouFnc() {
-    alert('Thank you for eating with us today!');
-  }
-  
+    .then(res => {
+        return res.json()
+    })
+    .then(data => {
+        items.innerHTML = `
+        <div class="item">
+        <div class="image">
+         <img src=${data[0].imgSrc} alt="">
+        </div>
+         <div class="desc">
+             <div class="lef">
+                 <p>${data[0].name}</p>
+                 <p>$ ${data[0].price}/-</p>
+             </div>
+             <div class="right">
+                 <i class="fa-solid fa-plus"></i>
+             </div>
+            
+         </div> 
+         
+     </div> 
+
+
+      <div class="item">
+         <div class="image">
+          <img src=${data[1].imgSrc} alt="">
+         </div>
+          <div class="desc">
+              <div class="lef">
+                  <p>${data[1].name}</p>
+                  <p>$${data[1].price}/-</p>
+              </div>
+              <div class="right">
+                  <i class="fa-solid fa-plus"></i>
+              </div>
+             
+          </div> 
+          
+      </div> 
+      
+      <div class="item">
+         <div class="image">
+          <img src=${data[2].imgSrc} alt="">
+         </div>
+          <div class="desc">
+              <div class="lef">
+                  <p>${data[2].name}</p>
+                  <p>$${data[2].price}/-</p>
+              </div>
+              <div class="right">
+                  <i class="fa-solid fa-plus"></i>
+              </div>
+             
+          </div> 
+          
+      </div> 
+        `
+    
+
+    })
+    
+}
+getMenu();
+
+function takeOrder() {
+    // console.log( window.location.href )
+    window.location.href = "https://shiva-saini.github.io/f3_c1/index1.html";
+    
+}
+
+
+
+function TakeOrderFunction() {
+    return new Promise((resolve,reject) => {
+           setTimeout(() => {
+             takeOrder();
+             resolve("success");
+           },2500)
+    })
+}
+
+let takeOrderData = TakeOrderFunction();
+
+takeOrderData.then(data => {
+  console.log(data);
+})
